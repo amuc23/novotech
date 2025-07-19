db7ff062ffad61341524509c5c5f237aa477fb38
amuc <amuc23@gmail.com>
2025-07-19 16:55:17 -0400

    cambios 8

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', views.home_view, name='index'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('maquinas/', views.maquinas, name='maquinas'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home/index.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
+
+def maquinas(request):
+    return render(request, 'maquinas.html')
